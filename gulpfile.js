b9cf510478dd5339a1cf991d9308f5e9cbdb1d76
whdlsrnr0731 <whdlsrnr0731@gmail.com>
2022-07-11 19:40:49 +0900

    update

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,9 @@ var gulp = require('gulp');
 
 // gulp plugins and utils
 var gutil = require('gulp-util');
-var livereload = require('gulp-livereload');
 var postcss = require('gulp-postcss');
 var sourcemaps = require('gulp-sourcemaps');
-var zip = require('gulp-zip');
+var imagemin = require('gulp-imagemin');
 
 // postcss plugins
 var autoprefixer = require('autoprefixer');
@@ -14,18 +13,10 @@ var cssnano = require('cssnano');
 var customProperties = require('postcss-custom-properties');
 var easyimport = require('postcss-easy-import');
 
-var swallowError = function swallowError(error) {
-    gutil.log(error.toString());
-    gutil.beep();
-    this.emit('end');
-};
-
-var nodemonServerInit = function () {
-    livereload.listen(1234);
-};
-
-gulp.task('build', ['css'], function (/* cb */) {
-    return nodemonServerInit();
+gulp.task('images', function() {
+    return gulp.src('assets/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('assets/built/images/'))
 });
 
 gulp.task('css', function () {
@@ -38,32 +29,8 @@ gulp.task('css', function () {
     ];
 
     return gulp.src('assets/css/*.css')
-        .on('error', swallowError)
         .pipe(sourcemaps.init())
         .pipe(postcss(processors))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('assets/built/'))
-        .pipe(livereload());
-});
-
-gulp.task('watch', function () {
-    gulp.watch('assets/css/**', ['css']);
-});
-
-gulp.task('zip', ['css'], function() {
-    var targetDir = 'dist/';
-    var themeName = require('./package.json').name;
-    var filename = themeName + '.zip';
-
-    return gulp.src([
-        '**',
-        '!node_modules', '!node_modules/**',
-        '!dist', '!dist/**'
-    ])
-        .pipe(zip(filename))
-        .pipe(gulp.dest(targetDir));
-});
-
-gulp.task('default', ['build'], function () {
-    gulp.start('watch');
 });
